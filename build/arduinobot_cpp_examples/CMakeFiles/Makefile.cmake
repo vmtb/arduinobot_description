34b8c744621a3a74ee89acd9e867bbe29ffdb317
vmtb <marcosvitouley@gmail.com>
2024-08-21 10:32:00 -0700

    ros controller yaml

--- build/arduinobot_cpp_examples/CMakeFiles/Makefile.cmake
+++ build/arduinobot_cpp_examples/CMakeFiles/Makefile.cmake
@@ -124,8 +124,6 @@ set(CMAKE_MAKEFILE_DEPENDS
   "/opt/ros/humble/share/ament_cmake_libraries/cmake/ament_cmake_librariesConfig-version.cmake"
   "/opt/ros/humble/share/ament_cmake_libraries/cmake/ament_cmake_librariesConfig.cmake"
   "/opt/ros/humble/share/ament_cmake_libraries/cmake/ament_libraries_deduplicate.cmake"
-  "/opt/ros/humble/share/ament_cmake_libraries/cmake/ament_libraries_pack_build_configuration.cmake"
-  "/opt/ros/humble/share/ament_cmake_libraries/cmake/ament_libraries_unpack_build_configuration.cmake"
   "/opt/ros/humble/share/ament_cmake_lint_cmake/cmake/ament_cmake_lint_cmake-extras.cmake"
   "/opt/ros/humble/share/ament_cmake_lint_cmake/cmake/ament_cmake_lint_cmakeConfig-version.cmake"
   "/opt/ros/humble/share/ament_cmake_lint_cmake/cmake/ament_cmake_lint_cmakeConfig.cmake"
